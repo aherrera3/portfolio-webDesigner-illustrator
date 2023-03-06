91b23e062e1f735c45551543d5885cdf91d34c94
Angélica Herrera Alba <angelicaherreraalba@gmail.com>
2023-03-06 09:07:30 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about-me')
+def about_me():
+    return render_template('about_me.html')
+
+@app.route('/portfolio')
+def portfolio():
+    return render_template('portfolio.html')
+
+@app.route('/portfolio/web-designer')
+def web_designer():
+    return render_template('web_designer.html')
+
+@app.route('/portfolio/illustrator')
+def illustrator():
+    return render_template('illustrator.html')
+
+@app.route('/contact-me')
+def contact_me():
+    return render_template('contact_me.html')
+
+if __name__ == '__main__':
+   app.run(debug = True)
